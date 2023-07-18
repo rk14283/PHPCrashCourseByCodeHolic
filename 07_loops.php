9d7d969ea0c43274ffd94a541c06d64912587d5b
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-18 14:10:00 +0100

    php crash course functions and dates

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,47 @@
 <?php
 
 // while
+// while (true){
 
+// }
 // Loop with $counter
-
+//$counter =0;
+// while($counter<10){
+//     echo $counter.'<br>';
+//     // if($counter ===5){
+//     //     break;
+//     // }
+//     $counter++; 
+// }
 // do - while
-
+//In do while condition is printed at-least once
+// do{
+//     echo $counter.'<br>';
+//     $counter++; 
+// }//in a while loop if this is changed to 0, it will never be printed 
+// while ($counter<0); 
 // for
-
+for($i=0; $i<10;$i++){
+    echo $i.'<br>'; 
+}
 // foreach
-
+$fruits = ["Banana", "Apple", "Orange"]; 
+foreach($fruits as $i=>$fruit){
+    echo $i. ' '. $fruit.'<br>';  
+ }
 // Iterate Over associative array.
+$person  = [
+    'name' => 'Brad',
+    'Surname'=> 'Traversy',
+    'age' => 30,
+    'hobbies'=>['Tennis', 'Video Games']
+]; 
+foreach($person as $key =>$value){
+    //array to string conversion would create an error without implode 
+    if(is_array($value)){
+        echo $key . ' '. implode(",", $value).'<br>'; 
+    }else{
+        echo $key . ' '.$value.'<br>';         
+    }
+
+}
--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,38 @@
 <?php
 
 // Function which prints "Hello I am Zura"
-
+// function hello(){
+//     echo "Hello I am Zura"; 
+// }
+// hello(); 
 // Function with argument
-
-// Create sum of two functions
-
+// function hello($name){
+// return "Hello I am $name"; 
+// }
+// echo hello('Zura').'<br>'; 
+// echo hello('Brad').'<br>'; 
+// // Create sum of two functions
+// function sum($a, $b){
+//     return $a + $b; 
+// }
+// echo sum(4,5); 
 // Create function to sum all numbers using ...$nums
+// function sum(...$nums){
 
+// $sum = 0; 
+// foreach($nums as $n){
+//     $sum += $n; 
+// }
+// return $sum; 
+// }
+// echo sum(1,2,3,4,5,6); 
 // Arrow functions
+//new feature added in PHP 
+function sum(...$nums)
+
+
+ {
+ return array_reduce($nums,fn($carry,$n) =>$carry +$n);
+ }
+
+echo sum(1,2,3,4,5,6); 
