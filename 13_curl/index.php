1ab04db21e8a43de49626670859ce476294fd359
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-19 15:47:58 +0100

    Basic CRUD

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -2,9 +2,42 @@
 
 $url = 'https://jsonplaceholder.typicode.com/users';
 // Sample example to get data.
+// $resource = curl_init($url);
+// curl_setopt($resource,CURLOPT_RETURNTRANSFER,true); 
+// $result = curl_exec($resource); 
+// curl_close($resource);
 
+// $code = curl_getinfo($resource,CURLINFO_HTTP_CODE);
+// //curl_getinfo($resource); 
+
+// echo '<pre>';
+// var_dump($code);
+// echo '</pre>';
+
+// echo $result; 
 // Get response status code
 
 // set_opt_array
 
 // Post request
+
+$resouce =curl_init();
+$user = [
+    'name'=>'John Doe',
+    'username' =>'john',
+    'email' =>'john@example.com'
+]; 
+
+$resource =curl_init();
+curl_setopt_array($resouce, [
+CURLOPT_URL =>$url, 
+CURLOPT_RETURNTRANSFER =>true,
+CURLOPT_POST =>true, 
+//without this there will be a wierd response 
+CURLOPT_HTTPHEADER =>['content-type: application/json'],
+
+CURLOPT_POSTFIELDS => json_encode($user)
+]);
+$result = curl_exec($resouce);
+curl_close($resouce); 
+echo $result; 
---
+++ 14_product_crud/index.php
@@ -0,0 +1,62 @@
+<?php
+//PDO to establish connection to database, add one more s and you will see an error 
+$pdo = new PDO('mysql:host=localhost; port=3306; dbname=products_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$statement = $pdo->prepare('SELECT*FROM products ORDER BY create_date DESC'); 
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+var_dump($products);
+
+echo '<pre>';
+var_dump($products);
+echo '<pre>';
+?>
+
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel = "app.css">
+    <title>Products CRUD</title>
+  </head>
+  <body>
+    <h1>Products CRUD</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+  </body>
+
+</html>
